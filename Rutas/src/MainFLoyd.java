2be3fd9636d8f1dd9dd975147cadeda3592f8dcc
FabianFallas <37458918+FabianFallas@users.noreply.github.com>
2018-11-25 23:58:02 -0600

    Main
    se agrega main para comprobarlos

---
+++ Rutas/src/MainFLoyd.java
@@ -0,0 +1,29 @@
+ import javax.swing.UIManager;
+ import javax.swing.UnsupportedLookAndFeelException;
+ public class MainFLoyd
+ {
+ public static void main(String args[])
+   {
+       try
+        {
+          UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (ClassNotFoundException e)
+          {
+             e.printStackTrace();
+          } catch (InstantiationException e)
+          {
+            e.printStackTrace();
+          } catch (IllegalAccessException e)
+          {
+            e.printStackTrace();
+          } catch (UnsupportedLookAndFeelException e)
+          {
+            e.printStackTrace();
+          }
+         Ventana miVentana;
+         Logica logica;
+ 
+         miVentana=new Ventana();
+         logica=new Logica();
+   }
+ }
--- Rutas/src/main.java
+++ Rutas/src/main.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-class principal{
+class MainDijkstra{
 
   public static void main(String args[]){
 
